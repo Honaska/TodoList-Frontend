87b29c0c5c82568b840ca8c750ed61a69d0f774a
Tantoy, James Ryan <100672289+Honaska@users.noreply.github.com>
2025-04-15 23:08:28 +0800

    Update api.js

--- todo-app/src/api.js
+++ todo-app/src/api.js
@@ -1,8 +1,9 @@
+// api.js
 import axios from 'axios';
 
-const BASE_URL = 'https://fastapi-6txf.onrender.com'; // change this to your actual backend URL
+const BASE_URL = 'https://fastapi-6txf.onrender.com';
 
-export const getTasks = () => axios.get(`${BASE_URL}/tasks/`);
-export const createTask = (task) => axios.post(`${BASE_URL}/tasks/`, task);
-export const updateTask = (id, task) => axios.put(`${BASE_URL}/tasks/${id}/`, task);
-export const deleteTask = (id) => axios.delete(`${BASE_URL}/tasks/${id}/`);
+export const getTasks = () => axios.get(`${BASE_URL}/tasks`);
+export const createTask = (task) => axios.post(`${BASE_URL}/tasks`, task);
+export const updateTask = (id, task) => axios.put(`${BASE_URL}/tasks/${id}`, task);
+export const deleteTask = (id) => axios.delete(`${BASE_URL}/tasks/${id}`);
